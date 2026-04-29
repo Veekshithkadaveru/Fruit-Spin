b9f92bd8f76bb1a33f7541f99d6275aa8d95933c
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2026-04-29 22:52:02 +0530

    Implement TapValidator for tap hit detection

---
+++ app/src/main/java/app/krafted/fruitspin/utils/TapValidator.kt
@@ -0,0 +1,35 @@
+package app.krafted.fruitspin.utils
+
+import app.krafted.fruitspin.viewmodel.Fruit
+import kotlin.math.abs
+
+object TapValidator {
+
+    private const val SEGMENT_ANGLE = 360f / 7f
+    private const val POINTER_ANGLE = 270f
+    private const val TOLERANCE = 15f
+
+    fun validateTap(rotationAngle: Float): Fruit? {
+        val fruits = Fruit.values()
+        
+        val normalizedRotation = ((rotationAngle % 360f) + 360f) % 360f
+        
+        for (i in fruits.indices) {
+            val fruitStartAngle = i * SEGMENT_ANGLE
+            val fruitCenterAngle = fruitStartAngle + (SEGMENT_ANGLE / 2f)
+            
+            val currentCenterAngle = (fruitCenterAngle + normalizedRotation) % 360f
+            
+            var diff = abs(currentCenterAngle - POINTER_ANGLE)
+            if (diff > 180f) {
+                diff = 360f - diff
+            }
+            
+            if (diff <= TOLERANCE) {
+                return fruits[i]
+            }
+        }
+        
+        return null
+    }
+}
